5bfa3b177e801892f5d486c2877aa86915160f78
TX <hitxtx@outlook.com>
2020-04-29 22:26:10 +0800

    fix: clear form fields

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -5,10 +5,10 @@ $(function () {
     $.fn.reset = function () {
         let _form = this;
         _form[0].reset();
+        _form.children('input[type="hidden"]').val("");
         let select2 = _form.find("select.select2");
         if (select2.length > 0) {
             select2.val("").trigger("change");
         }
-        $('input[type="hidden"]').val("");
     };
 });
