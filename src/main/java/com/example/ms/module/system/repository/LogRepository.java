6b43a92515a91bb565f3b39d6e7693b469d87cf3
TX <hitxtx@outlook.com>
2022-10-16 11:12:44 +0800

    reset port

--- src/main/java/com/example/ms/module/system/repository/LogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms.module.system.repository;
-
-import com.example.ms.module.system.entity.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-
-}
